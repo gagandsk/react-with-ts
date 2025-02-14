1832fad983204a4a1e5c91dfea2e2f58184ed96d
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-14 11:20:10 +0100

    ✨ El objeto 'props' y 'children'

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,14 +1,14 @@
-const randomNumber = () => Math.floor(Math.random() * 122) + 1;
 
-export function RandomFox() {
-  const image = `https://randomfox.ca/images/${randomNumber()}.jpg`;
+type Props = { image: string };
+
+export function RandomFox({ image }: Props) {
 
   return (
     <img
       width="320"
       height="auto"
       src={image}
-      className="mx-auto rounded-md bg-gray-300"
+      className="rounded"
     />
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { RandomFox } from "../../components/RandomFox";
 
+const randomNumber = () => Math.floor(Math.random() * 122) + 1;
+
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hola</h1>
-      <RandomFox />
+      <RandomFox image={`https://randomfox.ca/images/${randomNumber()}.jpg`}/>
     </>
   );
 }
