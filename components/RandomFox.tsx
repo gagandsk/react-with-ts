fd3d806f4faee572da79a6879cd8c20d018b138b
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-14 13:50:47 +0100

    ✨Tipos para referencias y observadores

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,10 +1,12 @@
-
+import { useRef } from "react";
 type Props = { image: string };
 
 export function RandomFox({ image }: Props) {
-
+  const node = useRef<HTMLImageElement>(null);
+  
   return (
     <img
+      ref={node}
       width="320"
       height="auto"
       src={image}
